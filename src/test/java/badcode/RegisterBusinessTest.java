7a9707b0250a1ccaf299eff7f2457f2cd93ab3e5
jassada.sir <jassada.sir@ntb.co.th>
2021-08-28 11:29:20 +0700

    test to pass

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -7,6 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterBusinessTest {
 
+    //test to pass
+    @Test
+    @DisplayName("สามารถบันทึกข้อมูล")
+    public void case0() {
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("firstName");
+        speaker.setLastName("lastName");
+        speaker.setEmail("email@gmail.com");
+        int speakerId = registerBusiness.register(new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1000;
+            }
+        }, speaker);
+        assertEquals(1000, speakerId);
+    }
+
+
+    // test to fail
     @Test
     @DisplayName("ไม่กำหนดชื่อ จะเกิด exception First name is required.")
     public void caseExceptionFirstName() {
@@ -55,7 +75,7 @@ class RegisterBusinessTest {
     }
 
     @Test
-    @DisplayName("ไม่กำหนด Domain จะเกิด exception Email Domain")
+    @DisplayName("ไม่กำหนด Email Domain จะเกิด exception Email Domain")
     public void caseExceptionEmailDomain() {
         RegisterBusiness registerBusiness = new RegisterBusiness();
         Speaker speaker = new Speaker();
@@ -81,11 +101,14 @@ class RegisterBusinessTest {
             registerBusiness.register(null,speaker);
             fail();
         } catch (SpeakerDoesntMeetRequirementsException e) {
+            if (!e.getMessage().equals("Speaker doesn't meet our standard rules.")) {
+                fail();
+            }
         }
     }
 
     @Test
-    @DisplayName("ไม่กำหนดชื่อ จะเกิด exception Can't save a speaker.")
+    @DisplayName("ไม่สามารถบันทึกข้อมูล")
     public void caseExceptionCanNotSaveASpeaker() {
         RegisterBusiness registerBusiness = new RegisterBusiness();
         Speaker speaker = new Speaker();
@@ -96,6 +119,9 @@ class RegisterBusinessTest {
             registerBusiness.register(null,speaker);
             fail();
         } catch (SaveSpeakerException e) {
+            if (!e.getMessage().equals("Can't save a speaker.")) {
+                fail();
+            }
         }
     }
 }
